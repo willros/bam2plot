6ef9ece4b9374721fee7c581b4a37c9a62a450c7
Williams UMU-MacBook <william.rosenbaum@umu.se>
2023-08-18 11:38:30 +0200

    setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="bam2plot",
-    version="0.1.0",
+    version="0.1.1",
     description="Plot of coverage from bam file",
     url="https://github.com/willros/bam2plot",
     author="William Rosenbaum",
