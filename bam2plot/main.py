364c0a1c2953666668a42cf847286fa0f2889b83
Williams UMU-MacBook <william.rosenbaum@umu.se>
2023-08-20 17:27:54 +0200

    format

--- bam2plot/main.py
+++ bam2plot/main.py
@@ -12,52 +12,42 @@ import os
 import pandas
 
 # for windows users
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 
 
 SORTED_TEMP = "TEMP112233.sorted.bam"
 SORTED_TEMP_INDEX = f"{SORTED_TEMP}.bai"
 
+
 def sort_bam(bam: str, new_name: str) -> None:
     pysam.sort("-o", new_name, bam)
-    
+
+
 def index_bam(bam: str, new_name: str) -> None:
     pysam.index(bam, new_name)
-    
-    
-    
-    
+
+
 def run_perbase(bam: str) -> _io.StringIO:
     return StringIO(
         subprocess.check_output(
-            f"perbase only-depth {bam}", 
-            shell=True, 
-            stderr=subprocess.DEVNULL
-        )
-        .decode()
+            f"perbase only-depth {bam}", shell=True, stderr=subprocess.DEVNULL
+        ).decode()
     )
+
 
 def perbase_to_df(perbase: _io.StringIO) -> pd.DataFrame:
-    return (
-        pd.read_csv(
-            perbase, 
-            sep="\t", 
-        )
-        .rename(columns={"REF": "id", "POS": "Position", "DEPTH": "coverage"})
-    )
-
+    return pd.read_csv(
+        perbase,
+        sep="\t",
+    ).rename(columns={"REF": "id", "POS": "Position", "DEPTH": "coverage"})
 
 
 def interpolate_df_ranges(df, rolling_window: int):
     stop = df.END.max()
-    df_to_interpolate = (
-        pd.DataFrame()
-        .assign(Position=range(1, stop + 1))
-    )
-    
+    df_to_interpolate = pd.DataFrame().assign(Position=range(1, stop + 1))
+
     return (
-        df
-        .merge(df_to_interpolate, on="Position", how="right")
+        df.merge(df_to_interpolate, on="Position", how="right")
         .drop(columns="END")
         .assign(coverage=lambda x: x.coverage.ffill())
         .assign(id=lambda x: x.id.ffill())
@@ -68,12 +58,11 @@ def interpolate_df_ranges(df, rolling_window: int):
 def wrangle_perbase_df(df, rolling_window: int):
     return pd.concat(
         [
-            interpolate_df_ranges(df.loc[lambda x: x.id == ref], rolling_window) 
+            interpolate_df_ranges(df.loc[lambda x: x.id == ref], rolling_window)
             for ref in df.id.unique()
         ],
-        ignore_index=True
+        ignore_index=True,
     )
-
 
 
 def plot_coverage(
@@ -111,31 +100,30 @@ def make_dir(outpath: str) -> None:
         outpath.mkdir(parents=True)
 
 
-
 def cli(
     bam: str,
     outpath: str = "",
     rolling_window: int = 100,
     threshold: int = 3,
     index: bool = False,
-    sort_and_index: bool = False
+    sort_and_index: bool = False,
 ) -> None:
     sample_name = Path(bam).stem
-        
+
     if outpath == "":
         outpath = "bam2plot"
         make_dir(outpath)
     else:
         make_dir(outpath)
-        
+
     out_file = f"{outpath}/{sample_name}_bam2plot"
-    
-        
+
     if sort_and_index:
         print("Sorting bam file")
         sort_bam(bam, new_name=SORTED_TEMP)
         print("Indexing bam file")
         index_bam(SORTED_TEMP, new_name=SORTED_TEMP_INDEX)
+        print("Running perbase")
         perbase = run_perbase(SORTED_TEMP)
         os.remove(SORTED_TEMP)
         os.remove(SORTED_TEMP_INDEX)
@@ -144,6 +132,7 @@ def cli(
             print("Indexing bam file")
             index_name = f"{bam}.bai"
             index_bam(bam, new_name=index_name)
+        print("Running perbase")
         perbase = run_perbase(bam)
         if index:
             os.remove(index_name)
