c675ce5a13c9e1bbd6b78a192776af283d96d7e9
Williams UMU-MacBook <william.rosenbaum@umu.se>
2024-03-12 10:19:50 +0100

    Added log_scale and versions and ignore errors to decode()

---
+++ Makefile
@@ -0,0 +1,4 @@
+clean:
+	rm -rf dist/ build/ *.egg-info
+build:
+	python -m build
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="bam2plot",
-    version="0.2.2",
+    version="0.2.3",
     description="Plot of coverage from bam file",
     url="https://github.com/willros/bam2plot",
     author="William Rosenbaum",
@@ -18,10 +18,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     python_requires=">=3.10",
     install_requires=[
-        "pandas",
-        "seaborn",
-        "pysam",
-        "fire",
+        "fire==0.6.0",
+        "pandas==2.2.1",
+        "pysam==0.22.0",
+        "seaborn==0.13.2",
     ],
     entry_points={"console_scripts": ["bam2plot=bam2plot.main:run"]},
 )
