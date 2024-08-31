c5a97ebba9f34aafa6e05931a15babeb7b4d44a4
Williams UMU-MacBook <william.rosenbaum@umu.se>
2024-08-31 15:56:17 +0200

    format and setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="bam2plot",
-    version="0.3.3",
+    version="0.3.4",
     description="Plot of coverage from bam file",
     url="https://github.com/willros/bam2plot",
     author="William Rosenbaum",
@@ -16,13 +16,14 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=[
         "pysam==0.22.0",
         "seaborn==0.13.2",
         "polars==0.20.15",
         "mappy==2.28",
         "pyfastx",
+        "pyarrow",
     ],
     entry_points={"console_scripts": ["bam2plot=bam2plot.main:cli"]},
 )
